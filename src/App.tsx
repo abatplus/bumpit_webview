f1a37837fd012e8e971818422605e0a2eb3b7ef3
Harald Schäfer <harald.schaefer@abatplus.de>
2020-06-29 09:53:39 +0200

    adding material ui package and get current locale from navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'fontsource-roboto';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
+  const locale = navigator.language;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Typography noWrap={true} variant={'h3'}>
+          Current locale: {locale}
+        </Typography>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. Current locale: {locale}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
